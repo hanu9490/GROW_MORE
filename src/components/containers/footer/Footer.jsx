3d112880b1d153a4ae01bf37698a25e19fdd1eb2
sai <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2024-07-07 13:10:17 +0530

    git connection test

---
+++ src/components/containers/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>© 2022 My Website</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
