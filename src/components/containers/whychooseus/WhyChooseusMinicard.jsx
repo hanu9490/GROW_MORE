5c82d003f6910e99793d415ff5477d93fd5d06e0
hanu9490 <hanumanthu9490@gmail.com>
2024-07-20 22:15:15 +0530

    commit why choose us component v1

--- src/components/containers/whychooseus/WhyChooseusMinicard.jsx
+++ src/components/containers/whychooseus/WhyChooseusMinicard.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { whyChooseusIcons } from "../../../assets/icons/icons";
 import "./WhyChooseusMinicard.css"
 
 const WhyChooseusMinicard = ({icon, title, description}) => {
   return (
     <div className="why-chooseus-minicard-container">
       <img src={icon} alt="Client-Centric" className="icon" />
-      <div className="text-content">
+      <div className="why-chooseus-textcon">
         <h3 className="title">{title}</h3>
         <p className="description">
           {description}
