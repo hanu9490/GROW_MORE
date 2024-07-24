b5ae8a63c00f60d82296cd9379f63492e082f44c
sai <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2024-07-24 23:38:30 +0530

    cleared conflicts and updated the code

--- src/components/containers/navbar/Navbar.jsx
+++ src/components/containers/navbar/Navbar.jsx
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-
-
-const Navbar = () => {
-  return (
-    <AppBar position="static" sx={{ bgcolor: 'white' }}>
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          {/* <MenuIcon /> */}
-          <img/>
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: 'black' }}>
-          Logo
-        </Typography>
-        <Box>
-          <Button color="inherit" sx={{ color: 'black' }}>Home</Button>
-          <Button color="inherit" sx={{ color: 'black' }}>About</Button>
-          <Button color="inherit" sx={{ color: 'black' }}>Contact</Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
-}
-
-=======
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import { navbarIcons } from "../../../assets/icons/icons";
@@ -86,5 +53,4 @@ const Navbar = () => {
   );
 };
 
->>>>>>> 8d34c8c2aa27450585cee4cb47454539add249f1
 export default Navbar;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,39 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import WhatWeDo from "../components/containers/whatwedo/WhatWeDo";
 import WhyOurServices from "../components/containers/whyourservices/WhyOurServices";
-<<<<<<< HEAD
-import WhyChooseus from '../components/containers/whychooseus/WhyChooseus';
-import ContactUs from '../components/containers/contactus/ContactUs';
-import Internship from '../components/containers/internship/Internship';
-
-const routes = (
-    <React.Fragment>
-        <Routes>
-            <Route element={<Home />} path='/' />
-            <Route element={<WhatWeDo/>} path='/whatwedo' />
-            <Route element={<WhyOurServices/>} path='/whyourservices' />
-            <Route element={<WhyChooseus/>} path='/whychooseus' />
-            <Route element={<ContactUs/>} path='/contactus' />
-            <Route element={<Internship/>} path='/internship' />
-        </Routes>
-    </React.Fragment>
-)
-export default routes
-=======
 import WhyChooseus from "../components/containers/whychooseus/WhyChooseus";
 import ContactUs from "../components/containers/contactus/ContactUs";
 import Navbar from "../components/containers/navbar/Navbar";
 import Hero from "../components/containers/hero/Hero";
+import WhatWeDo from "../components/containers/whatwedo/WhatWeDo";
+import Internship from "../components/containers/internship/Internship";
 
 const routes = (
   <React.Fragment>
     <Navbar />
     <Hero />
+    <WhatWeDo />
     <WhyOurServices />
     <WhyChooseus/>
+    <Internship />
     <ContactUs />
   </React.Fragment>
 );
 export default routes;
->>>>>>> 8d34c8c2aa27450585cee4cb47454539add249f1
