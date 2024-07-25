2bf0c0bb436c72f32977678a65e65a7d29c26b2b
hanu9490 <hanumanthu9490@gmail.com>
2024-07-25 22:00:08 +0530

    testimonials v1

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,16 +7,19 @@ import Navbar from "../components/containers/navbar/Navbar";
 import Hero from "../components/containers/hero/Hero";
 import WhatWeDo from "../components/containers/whatwedo/WhatWeDo";
 import Internship from "../components/containers/internship/Internship";
+import Testimonials from "../components/containers/testimonials/Testimonials";
 
 const routes = (
   <React.Fragment>
+    {/* <Navbar /> */}
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/whatwedo" element={<WhatWeDo />} />
       <Route path="/whyourservices" element={<WhyOurServices />} />
       <Route path="/whychooseus" element={<WhyChooseus />} />
       <Route path="/internship" element={<Internship />} />
-      <Route path="/contactus" element={<ContactUs />} />  
+      <Route path="/contactus" element={<ContactUs />} />
+      <Route path="/testimonial" element={<Testimonials />} />
     </Routes>
   </React.Fragment>
 );
