66781bc4e07a81f0bb8571dd9fe354fdc1916206
hanu9490 <hanumanthu9490@gmail.com>
2024-07-25 22:47:05 +0530

    why our services v2

--- src/components/containers/whyourservices/ServicesMinicard.jsx
+++ src/components/containers/whyourservices/ServicesMinicard.jsx
@@ -4,7 +4,7 @@ import "./ServicesMinicard.css";
 const ServicesMinicard = ({ data }) => {
   const { title, img } = data;
   return (
-    <div className="minicard-container">
+    <div className="why-our-services-minicard">
       <img src={img} alt="WhyourServices.png3" />
       <h2 className="minicard-title">{title}</h2>
     </div>
