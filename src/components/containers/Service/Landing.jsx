08f098a15aeb5f55116ae507dd6c3eea7ad35424
Harish <harishrockz6305@gmail.com>
2024-08-15 22:41:37 +0530

    services

---
+++ src/components/containers/Service/Landing.jsx
@@ -0,0 +1,42 @@
+import DataScience from "./DataScience";
+import MobileApplication from "./MobileApplication";
+import MoreServices from "./MoreSerices";
+import ProjectApproach from "./ProjectApproach";
+import "./Services.css"
+import WebApplication from "./webApp";
+const Landing = () => {
+
+    return (
+        <div>
+            <div className="container">
+                <div className="container-overLayer">
+                    <h1 className="lander-header">Services
+                    </h1>
+                    <span className="breadcrumb">Home / Services</span>
+                </div>
+            </div>
+            <div>
+                <h1 className="service-header">
+                    Exceptional Service
+                </h1>
+                <p className="service-description">
+                    Our commitment to delivering top-tier IT solutions is matched only
+                    by our dedication to exceptional customer service.
+                    From pre-sales consultation to post-implementation support,
+                    we prioritize your needs and ensure your satisfaction.
+                    Experience the difference of working with a team that
+                    goes above and beyond to exceed expectations.
+                </p>
+            </div>
+            <div>
+                <WebApplication />
+                <DataScience />
+                <MobileApplication />
+                <MoreServices />
+                <ProjectApproach />
+            </div>
+
+        </div>
+    );
+};
+export default Landing
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,14 +4,15 @@ import Navbar from "../components/containers/navbar/Navbar";
 import Home from "../components/screens/Home";
 import Carriers from "../components/screens/Carriers";
 import Contact from "../components/screens/Contact";
-
+import Services from "../components/screens/Services"
 const routes = (
   <React.Fragment>
-    <Navbar />
+    <Navbar/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/carriers" element={<Carriers />} />
       <Route path="/contactus" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
     </Routes>
   </React.Fragment>
 );
