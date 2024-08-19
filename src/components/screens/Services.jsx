b24a51324f56f9a765b570aaf1c31cd5d21e9e42
sai <SaiKumarNavara@tcsdtretail.onmicrosoft.com>
2024-08-19 21:51:21 +0530

    fixed merge conflits from harish pull

--- src/components/screens/Services.jsx
+++ src/components/screens/Services.jsx
@@ -1,10 +1,12 @@
 import Landing from "../containers/Service/Landing";
-import "../containers/Service/Services.css"
+import "../containers/Service/Services.css";
+import ScrollButton from "../common/ScrollToTop/ScrollUp";
 
 const Service = () => {
   return (
     <div>
       <Landing />
+      <ScrollButton />
     </div>
   );
 };
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,14 +5,25 @@ import Home from "../components/screens/Home";
 import Carriers from "../components/screens/Carriers";
 import Contact from "../components/screens/Contact";
 import Services from "../components/screens/Services";
+import NotFound from "../components/common/NotFound/NotFound";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
 const routes = (
   <React.Fragment>
+    <ScrollToTop />
     <Navbar />
     <Routes>
+      <Route path="*" element={<NotFound />} />
       <Route path="/" element={<Home />} />
       <Route path="/carriers" element={<Carriers />} />
       <Route path="/contactus" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
       <Route path="/services" element={<Services />} />
     </Routes>
   </React.Fragment>
