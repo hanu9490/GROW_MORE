486a230ae21986212386725f1d569f25f8caa05d
Harish <harishrockz6305@gmail.com>
2024-09-11 23:50:41 +0530

    service page Responsive

--- src/components/screens/Contact.jsx
+++ src/components/screens/Contact.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ContactUs from "../containers/contactus/ContactUs";
-// import Footer from "../containers/footer/Footer";
+//  import Footer from "../containers/footer/Footer";
 import ScrollButton from "../common/ScrollToTop/ScrollUp";
-import Map from "../containers/google-map/Map";
+// import Map from "../containers/google-map/Map";
 
 const Contact = () => {
   return (
     <div>
       <ContactUs />
-      <Footer />
+      {/* <Footer /> */}
       <ScrollButton />
     </div>
   );
